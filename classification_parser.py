e3538e0de949b300199f5c3beb36141c66ab5a66
philiphristov <filip.svetlinov.hristov@gmail.com>
2020-06-16 16:02:37 +0200

    dasdas

--- classification_parser.py
+++ classification_parser.py
@@ -11,7 +11,7 @@ from PIL import Image
 
 parsed_data = []
 
-df = pandas.read_csv("va-classifications.csv")
+df = pandas.read_csv("verbaalpina-classifications.csv")
 annotations = df["annotations"]
 subject_data = df["subject_data"]
 
@@ -21,48 +21,52 @@ for index, row in df.iterrows():
 	values = annotation["value"]
 
 	subject_key, subject_value =  next(iter( subject_data.items() ))
-	image_name = "subject_data/" + subject_value["current_subject_number"]
 
-	name = subject_value["current_subject_number"].replace(".jpg", "")
-	file_name = "current_results/" +  name + "-(current markings)" + ".png"
+	if "current_subject_number" in subject_value:
+		image_name = "subject_data/" + subject_value["current_subject_number"]
 
-	# pillow
-	im = Image.open(image_name)
-	im_width, im_height = im.size
+		name = subject_value["current_subject_number"].replace(".jpg", "")
+		file_name = "current_results/" +  name + "-(current markings)" + ".png"
 
-	# ploting
-	img = matplotlib.image.imread(image_name)
-	figure, ax = plt.subplots(1)
-	ax.imshow(img)
+		# pillow
+		im = Image.open(image_name)
+		im_width, im_height = im.size
 
-	print(name)
+		# ploting
+		img = matplotlib.image.imread(image_name)
+		figure, ax = plt.subplots(1)
+		ax.imshow(img)
 
-	for key, value in enumerate(values):
-		current_x = value["x"]
-		current_y = value["y"]
-		current_width = value["width"]
-		current_height = value["height"]
+		for key, value in enumerate(values):
+			current_x = value["x"]
+			current_y = value["y"]
+			current_width = value["width"]
+			current_height = value["height"]
 
-		detail_number = value["details"][0]["value"]
-		detail_transcription = value["details"][1]["value"]
-		print(detail_number, detail_transcription)
+			detail_number = value["details"][0]["value"]
+			detail_transcription = value["details"][1]["value"]
 
-		if detail_number is not "" and detail_transcription is not "":
-			parsed_data.append({"subject_id": subject_value["current_subject_number"], "key": str(key), "map_number":detail_number , "transcription":detail_transcription})
-		# plot visualisation
-		rect = matplotlib.patches.Rectangle((current_x,current_y),current_width,current_height, edgecolor='r', facecolor="none")
-		ax.add_patch(rect)
+			if detail_number != "" and detail_transcription != "":
+				print("map nr: {}".format(detail_number))
+				map_id = subject_value["current_subject_number"].split("#")[1]
+				map_id = map_id.split("-")[0]
 
-		# pillow cropping
-		(left, upper, right, lower) =  ( current_x, current_y, current_x + current_width , current_y + current_height )
-		im_crop = im.crop(box=(left, upper, right, lower))
-		file_name = "current_results/" +  name + "-" + str(key)+ ".png"
-		im_crop.save(file_name, quality=95)
+				parsed_data.append({"subject_id": subject_value["current_subject_number"], "key": str(key),"map_id":map_id, "map_number":detail_number, "transcription":detail_transcription})
 
-	figure.show()
+			# plot visualisation
+			rect = matplotlib.patches.Rectangle((current_x,current_y),current_width,current_height, edgecolor='r', facecolor="none")
+			ax.add_patch(rect)
+
+			# pillow cropping
+			(left, upper, right, lower) =  ( current_x, current_y, current_x + current_width , current_y + current_height )
+			im_crop = im.crop(box=(left, upper, right, lower))
+			file_name = "current_results/" +  name + "-" + str(key)+ ".png"
+			im_crop.save(file_name, quality=95)
+
+		figure.show()
 
 data_frame = pandas.DataFrame(parsed_data)
-data_frame.to_csv("training_data.csv")
+data_frame.to_csv("verbaalpina-zooniverse-transcription.csv", sep=";")
 
 	# save plot
 	# name = subject_value["current_subject_number"].replace(".jpg", "")
